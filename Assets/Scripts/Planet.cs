adb61643fef9228685d81dfe827696dcc43e212b
Chamuth Chamandana <c.chamandana@gmail.com>
2022-05-10 18:08:21 +0530

    before scatter

---
+++ Assets/Scripts/Planet.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class Planet : MonoBehaviour
+{
+  [Header("Properties")]
+  public string Name;
+}
---
+++ Assets/Scripts/ScatterEngine/PlanetScatter.cs
@@ -0,0 +1,22 @@
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEditor;
+//using UnityEngine;
+
+//public class PlanetScatter : EditorWindow
+//{
+//  GameObject[] SmallRocks;
+//  GameObject[] VerticalRockFormations;
+  
+//  [MenuItem("Tools/Planet Scatter")]
+//  public static void ShowWindow()
+//  {
+//    GetWindow(typeof(PlanetScatter));
+//  }
+
+//  private void OnGUI()
+//  {
+//    GUILayout.Label("Basic Settings");
+//    EditorGUILayout.
+//  }
+//}
